b7d3fdcc001fa50edc751af0ec2888d72dfcd174
Rogério Rodrigues <rogeriorodrigues@outlook.com>
2025-03-13 18:03:35 -0300

    Aula 3, consumo rest

---
+++ Aula3/Aula3/Aula3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Aula3.DTO;
+using Newtonsoft.Json;
+
+namespace Aula3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnFetchDataClicked(object sender, EventArgs e)
+        {
+            string url = "https://viacep.com.br/ws/"+ txtCep.Text.ToString() +"/json/";
+            var result = await FetchDataFromApi(url);
+            ResultLabel.Text = result;
+        }
+
+        private async Task<string> FetchDataFromApi(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var data = JsonConvert.DeserializeObject<ViaCepDTO>(response);
+                return
+                    $"CEP: {data.Cep}, Logradouro: {data.Logradouro}, Bairro: {data.Bairro}, Localidade: {data.Localidade}, UF: {data.Uf}";
+            }
+        }
+    }
+}
